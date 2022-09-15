2c64b35d50a18d33dc18e67c52473f846ef62bca
isacchr <isac.christiansson@gmail.com>
2022-09-15 10:18:55 +0200

    structure in place, ready to go

---
+++ exercises.js
@@ -0,0 +1,13 @@
+console.log('hi');
+
+function exercise1() {
+    console.log('hi from btn');
+
+    var txtBox1Value = document.getElementById('txt1').value;
+    var txtBox2Value = document.getElementById('txt2').value;
+
+    console.log(`${txtBox1Value}, ${txtBox2Value} ${txtBox1Value.length}`);    
+    console.log(txtBox1Value,txtBox2Value);
+
+
+}
