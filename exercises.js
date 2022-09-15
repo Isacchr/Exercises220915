5f2fb55c680e0f9310f427c448284e8c70ff014a
isacchr <isac.christiansson@gmail.com>
2022-09-15 15:00:30 +0200

    exercise 1 and 2 works, 3 does not really work

--- exercises.js
+++ exercises.js
@@ -1,13 +1,52 @@
-console.log('hi');
 
 function exercise1() {
-    console.log('hi from btn');
 
-    var txtBox1Value = document.getElementById('txt1').value;
-    var txtBox2Value = document.getElementById('txt2').value;
+    var txtBoxValue = document.getElementById('txt1').value;
+    const words = txtBoxValue.split(' ');
 
-    console.log(`${txtBox1Value}, ${txtBox2Value} ${txtBox1Value.length}`);    
-    console.log(txtBox1Value,txtBox2Value);
+    words.forEach(input => {
+        console.log(`${input}, ${input.length}`);
+    });
 
+
+}
+
+function exercise2() {
+
+    var txtBoxValue = document.getElementById('txt1').value;
+    const words = txtBoxValue.split(' ');
+    const reversed_words = txtBoxValue.split(' ');
+    
+
+
+    sortedwords = words.sort((a, b) => b.length - a.length);
+    sortedwords.forEach(input => {
+        console.log(`${input}`);
+    })
+
+    reversedwords = reversed_words.reverse();
+    reversedwords.forEach(input => {
+        console.log(`${input}`);
+    })
+
+
+}
+
+function exercise3 () {
+
+    var txtBoxValue = document.getElementById('txt1').value;
+    const words = txtBoxValue.split(' ');
+
+    let obj = {};
+
+        for(var i = 0; i < words.length; i++) {
+            if (obj[words[i]] == undefined) {
+                obj[words[i]] = 1;
+            } else {
+                obj[words[i]]++;
+            }
+        }
+
+        Object.values(obj).forEach((freq, word) => console.log(`${words[word]} ${freq}`));
 
 }
